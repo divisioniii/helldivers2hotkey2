b37ca86a37849b53024d7586c1f1906b29c45d3c
Dave McRee <davemcree@gmail.com>
2026-04-04 16:52:57 +1000

    code update working towards ficing keyb detection.

--- src/Helldivers2Hotkey2.cpp
+++ src/Helldivers2Hotkey2.cpp
@@ -191,9 +191,13 @@ int main() {
     // xkb setup (optional, for key names/printing)
     struct xkb_context *ctx = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
     struct xkb_rule_names names{};
-    names.rules = "evdev";
-    names.model = "pc105";
-    names.layout = "us";
+    names.rules  = getenv("XKB_DEFAULT_RULES");
+    names.model  = getenv("XKB_DEFAULT_MODEL");
+    names.layout = getenv("XKB_DEFAULT_LAYOUT");
+
+    if (!names.rules)  names.rules  = "evdev";
+    if (!names.model)  names.model  = "pc105";
+    if (!names.layout) names.layout = "us";
 
     struct xkb_keymap *keymap = xkb_keymap_new_from_names(ctx, &names, XKB_KEYMAP_COMPILE_NO_FLAGS);
     struct xkb_state *state = xkb_state_new(keymap);
